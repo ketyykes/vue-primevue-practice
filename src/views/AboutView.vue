f85c1008c45cb6b5da9978d38f863c722260910f
danny <xi4869@gmail.com>
2024-10-08 18:38:02 +0800

    Add EmailForm and EmailForm2 components to AboutView

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,13 @@
+<script setup>
+import EmailForm from '../components/EmailForm.vue';
+import EmailForm2 from '../components/EmailForm2.vue';
+</script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <EmailForm />
+    <EmailForm2 />
   </div>
 </template>
 
