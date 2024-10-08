0f3608e3ee848bd69a88d2f419d6e24c8b11ac6a
danny <xi4869@gmail.com>
2024-10-08 18:37:46 +0800

    Add EmailForm component

---
+++ src/components/EmailForm.vue
@@ -0,0 +1,52 @@
+<template>
+  <form @submit="onSubmit">
+    <pre>{{ values }}</pre>
+    <label for="email">Email</label>
+    <input id="email" type="text" name="email" v-model="email" :emailAttrs />
+    <span class="error"> {{ errors.email }}</span>
+
+    <label for="password">Password</label>
+    <input id="password" type="text" name="password" v-model="password" :passwordAttrs />
+    <span class="error">{{ errors.password }}</span>
+
+    <button type="submit">Submit</button>
+  </form>
+</template>
+<script setup>
+import { toTypedSchema } from '@vee-validate/zod';
+import { useForm } from 'vee-validate';
+import { z } from 'zod';
+
+const validationSchema = toTypedSchema(
+  z.object({
+    email: z.string().min(1, 'This is required').email('It needs to be an email'),
+    password: z.string().min(8, 'Too short')
+  })
+);
+
+const { handleSubmit, errors, values, defineField } = useForm({ validationSchema });
+
+const [email, emailAttrs] = defineField('email', {
+  validateOnModelUpdate: false
+});
+const [password, passwordAttrs] = defineField('password', {
+  validateOnModelUpdate: false
+});
+
+const onSubmit = handleSubmit((values) => {
+  alert(JSON.stringify(values, null, 2));
+});
+</script>
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  max-width: 15rem;
+  margin: 0 auto;
+}
+
+.error {
+  color: red;
+}
+</style>
